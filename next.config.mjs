79ab5941fcb72f9ea780c52040c32e800c8ce862
Pallav <jain.pallav1998@gmail.com>
2024-06-18 19:43:45 +0530

    refactor

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingExcludes: {
-    "*": ["*/vendored/contexts/amp-context.shared-runtime.js"],
-  },
+  // outputFileTracingExcludes: {
+  //   "*": ["*/vendored/contexts/amp-context.shared-runtime.js"],
+  // },
 };
 
 export default nextConfig;
